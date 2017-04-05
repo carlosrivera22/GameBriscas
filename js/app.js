e9afbd3355783c50b49b55ce33eaabe48cb3f127
carlosrivera22 <carlos.rivera22@upr.edu>
2017-04-05 13:05:25 -0400

    Briscas
    Briscas

---
+++ js/app.js
@@ -0,0 +1,82 @@
+angular.module('briscasApp', [])
+  .controller('BriscasController', function($scope) {
+
+          //array of cards
+          $scope.deck = ["/img/0.png","img/Bastos1.gif","img/Bastos2.gif","img/Bastos3.gif","img/Bastos4.gif","img/Bastos5.gif","img/Bastos6.gif",
+          "img/Bastos7.gif","img/Bastos8.gif","img/Bastos9.gif","img/Bastos10.gif","img/Bastos11.gif","img/Bastos12.gif"];
+
+          $scope.stack = [];
+          $scope.hand1 = [];
+          $scope.hand2 = [];
+          $scope.play = [];
+          $scope.life;
+          var turn1 = true;
+          var turn2 = false;
+
+          //function that fills the deck stack
+          $scope.fill_stack = function(){
+
+            while($scope.deck.length > 1){
+                do{
+                var rand = Math.floor((Math.random() * $scope.deck.length));
+              }
+              while(rand == 0);
+              $scope.stack.push($scope.deck[rand]);
+              $scope.deck.splice(rand,1);
+
+            }
+
+            $scope.getHand1();
+            $scope.getHand2();
+            $scope.getLife();
+
+          }
+
+          //life card
+          $scope.getLife = function(){
+            $scope.life = $scope.stack.pop();
+          }
+
+          //player 1 hand
+          $scope.getHand1 = function(){
+            for(i=0;i<3;i++){
+              $scope.hand1.push($scope.stack.pop());
+            }
+
+          }
+
+          //player 2 hand
+          $scope.getHand2 = function(){
+            for(i=0;i<3;i++){
+              $scope.hand2.push($scope.stack.pop());
+            }
+
+          }
+
+          $scope.makePlay = function(card){
+              $scope.play.push(card);
+              console.log($scope.play);
+              $scope.switchTurns()
+          }
+
+            $scope.getTurn1 = function(){
+              return turn1;
+            }
+
+            $scope.getTurn2 = function(){
+              return turn2;
+            }
+
+            $scope.switchTurns = function(){
+              if(turn2 == false){
+                turn2 = true;
+                turn1 = false;
+              }else{
+                turn2 = false;
+                turn1 = true;
+              }
+
+          }
+
+
+      });
